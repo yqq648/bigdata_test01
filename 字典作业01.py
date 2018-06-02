07e6c7f0936712e4dd2aa42a02f2a066c634b794
yq <648500318@qq.com>
2018-06-03 07:07:16 +0800

    commit first
    Signed-off-by: yq <648500318@qq.com>

---
+++ 字典作业01.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 16:22:25 2018
+
+@author: yq
+"""
+import random
+random.random()*100
+
+w={'温度':[23,34,32,19,26],
+   '情况':['多云','小雨','晴','阵雨','晴']}
+print (max(w['温度']))
+
+
+
+
+
+
+
+
+
